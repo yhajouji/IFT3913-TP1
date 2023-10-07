f5ec386cf1f12d03e40fca0ca42a964349752ffc
yhajouji <yassine.hajouji@gmail.com>
2023-10-06 21:38:13 -0400

    Lecture d'un fichier avec Path

--- Tloc/src/Tloc.java
+++ Tloc/src/Tloc.java
@@ -1,6 +1,58 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Tloc {
 
     public static void main(String[] args) {
 
+        Path chemin = Paths.get("C:/WorkSpace/fichierTest.txt");
+        //String filename = chemin.getFileName().toString();
+        System.out.println(tloc(chemin));
+        /*
+        if (args.length != 1) {
+            System.out.println("Error : 1 argument manquant");
+        } else {
+
+            //Path chemin = Paths.get(args.toString());
+            //String filename = chemin.getFileName().toString();
+            //System.out.println(tloc(filename));
+
+        }
+        */
+
+    }
+
+    public static int tloc (Path fileName){
+
+        int valeurTloc = 0;
+        System.out.println(fileName);
+
+        try  {
+            //System.out.println(fileName);
+
+            BufferedReader breader = Files.newBufferedReader(fileName);
+
+
+            String line;
+
+            while ((line = breader.readLine()) != null){
+                line.trim();
+                valeurTloc++;
+                System.out.println(line);
+            }
+
+            breader.close();
+
+        }catch (IOException e){
+            System.out.println("Erreur : " + e.getMessage());
+        }
+
+        return valeurTloc;
     }
 }
+
