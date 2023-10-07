8bc4682158932e4bcbdcc8fce017eeec104d39ce
yhajouji <yassine.hajouji@gmail.com>
2023-10-06 23:03:11 -0400

    Lecture de fichier avec Path

--- Tassert/src/Tassert.java
+++ Tassert/src/Tassert.java
@@ -1,6 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Tassert {
 
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("Error : chemin du fichier manquant");
+        } else {
+
+            Path chemin = Paths.get(args[0].toString());
+            System.out.println(tassert(chemin));
+
+        }
     }
+
+    public static int tassert (Path fileName){
+
+        int valeurTassert = 0;
+
+        try  {
+
+            BufferedReader breader = Files.newBufferedReader(fileName);
+
+            String line;
+
+            while ((line = breader.readLine()) != null){
+
+                line.trim();
+
+
+
+            }
+
+            breader.close();
+
+        }catch (IOException e){
+            System.out.println("Erreur : " + e.getMessage());
+        }
+
+        return valeurTassert;
+    }
+
 }
